e4d53e32039120608f0e918eae685a64765c1edc
Nitay <118196923+NitayGitHub@users.noreply.github.com>
2025-04-05 17:05:04 +0300

    examples["conditioning_image"]] not _images

--- examples/text_to_image/train_text_to_image.py
+++ examples/text_to_image/train_text_to_image.py
@@ -828,7 +828,7 @@ def main():
     def preprocess_train(examples):
         images = [image.convert("RGB") for image in examples[image_column]]
         examples["pixel_values"] = [train_transforms(image) for image in images]
-        conditioning_images = [image.convert("RGB") for image in examples["conditioning_images"]]
+        conditioning_images = [image.convert("RGB") for image in examples["conditioning_image"]]
         examples["mask_values"] = [mask_transforms(image) for image in conditioning_images]
         examples["input_ids"] = tokenize_captions(examples)
         return examples
